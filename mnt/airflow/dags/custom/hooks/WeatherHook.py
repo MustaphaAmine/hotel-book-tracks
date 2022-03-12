310b40826e9104b614ee33484ae4add3c266f1d7
MustaphaAmine <fm_debbih@esi.dz>
2022-03-12 22:20:24 +0100

    Adding Weather collector Operator and its hook

--- mnt/airflow/dags/custom/hooks/WeatherHook.py
+++ mnt/airflow/dags/custom/hooks/WeatherHook.py
@@ -1,18 +1,22 @@
 import requests
+import json
+import pandas as pd
+
 from airflow.hooks.base_hook import BaseHook
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
 
 class WeatherHook(BaseHook):
     DEFAULT_SCHEMA = "https"
 
-    def __init__(
-        self, conn_id, retry=3
-    ):
+    def __init__(self, conn_id, gcs_conn_id, retry=3):
         super().__init__()
         self._retry = retry
         self._conn_id = conn_id
+        self._gcs_conn_id = gcs_conn_id
 
         self._session = None
         self._base_url = None
+        self._headers = None
 
     def __enter__(self):
         return self
@@ -23,9 +27,51 @@ class WeatherHook(BaseHook):
     def close(self):
         self._session = None
         self._base_url = None
+        self._headers = None
 
     def get_conn(self):
-        raise NotImplementedError()
+        config = self.get_connection(self._conn_id)
+        schema = config.schema or self.DEFAULT_SCHEMA
+        host = config.host
+        password = config.password
+        self._base_url = f"{schema}://{host}/"
+        self._headers = {"x-rapidapi-host": host, "x-rapidapi-key": password}
+        self._session = requests.session()
+        return self._session, self._base_url, self._headers
 
-    def _get_weather(self):
-        raise NotImplementedError()
+    def get_weather_all_locations(self):
+        session, base_url, headers = self.get_conn()
+        for location in self._get_location_from_gcs():
+            params = {
+                "q": f"%s,%s" % (location["city_name"], location["countryCode"])
+            }
+            yield from self._get_weather_by_location(
+                session, base_url, headers, params, location["dest_id"]
+            )
+
+    def _get_weather_by_location(
+        self, session, base_url, headers, params, dest_id, endpoint="forecast"
+    ):
+        """
+        In this example we're going to be using an endpoint that forcasts weather
+        related data for 5 days every 3 hours, we are gonna make use only of the data the first day.
+        """
+        location_weather_info = pd.DataFrame(
+            session.request(
+                "GET", base_url + endpoint, headers=headers, params=params
+            ).json()["list"]
+        )[["main","wind","visibility","dt_txt"]]
+        location_weather_info.loc[:, "dest_id"] = dest_id
+        yield from location_weather_info.to_dict("records")
+
+    def _get_location_from_gcs(self):
+        """
+        Downlaod location information from the Google Cloud Storage Bucket
+        """
+        gcs_hook = GCSHook(self._gcs_conn_id)
+        locations_bytes = gcs_hook.download(
+            bucket_name="hotel-book-track-bucket",
+            object_name="locations/locations.json",
+        )
+        locations = json.loads(locations_bytes.decode("utf8"))
+        return locations
---
+++ mnt/airflow/dags/custom/operators/WeatherToGCSOperator.py
@@ -0,0 +1,57 @@
+import json
+import os
+import logging
+import tempfile
+import pandas as pd
+
+from datetime import timedelta
+from custom.hooks.WeatherHook import WeatherHook
+
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+
+
+class WeatherToGCSOperator(BaseOperator):
+    @apply_defaults
+    def __init__(
+        self,
+        w_conn_id="weather_api_conn",
+        gcp_conn_id="google_cloud_default",
+        bucket_name="hotel-book-track-bucket",
+        bucket_weather_subdir="weather-data",
+        *args,
+        **kwargs,
+    ):
+        super(WeatherToGCSOperator, self).__init__(**kwargs)
+        self._conn_id = w_conn_id
+        self._gcp_conn_id = gcp_conn_id
+        self._bucket_name = bucket_name
+        self._bucket_weather_subdir = bucket_weather_subdir
+
+    def execute(self, context):
+        weather_hook = WeatherHook(conn_id=self._conn_id, gcs_conn_id=self._gcp_conn_id)
+        gcs_hook = GCSHook(self._gcp_conn_id)
+        try:
+            locations_weather = pd.DataFrame(
+                list(weather_hook.get_weather_all_locations())
+            )
+            with tempfile.TemporaryDirectory() as tmp_dir:
+                tmp_path = os.path.join(tmp_dir, "weather.json")
+                with open(tmp_path, "w", encoding="utf-8") as f:
+                    json.dump(
+                        locations_weather[
+                            pd.to_datetime(locations_weather.dt_txt).dt.strftime("%Y-%m-%d")
+                            == context["data_interval_end"].strftime("%Y-%m-%d")
+                        ].to_dict("records"),
+                        f,
+                        ensure_ascii=False,
+                    )
+                gcs_hook.upload(
+                    bucket_name=self._bucket_name,
+                    object_name=f"{self._bucket_weather_subdir}/weather_%s.json"
+                    % context["data_interval_end"].strftime("%Y-%m-%d"),
+                    filename=tmp_path,
+                )
+        finally:
+            weather_hook.close()
